9fb09cce42244b0483eb211eedf1c3752c461a4b
santhoshkumar <santhoshkumar.elumalai@softsuave.com>
2023-06-08 22:09:00 +0530

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload and santhosh.
+          Edit <code>src/App.js</code> and save to reload and softsuave.
         </p>
         <a
           className="App-link"
