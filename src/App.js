9181ec8a99cc0486482634de273c9be6480d77ac
santhoshkumar <santhoshkumar.elumalai@softsuave.com>
2023-06-09 11:57:07 +0530

    added new change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload and softsuave.
+          Edit <code>src/App.js</code> and save to reload and softsuave new change.
         </p>
         <a
           className="App-link"
