78d8d8beb7f0c5c7d17c9622905d7236c2edf85e
santhoshkumar <santhoshkumar.elumalai@softsuave.com>
2023-06-16 14:39:05 +0530

    test

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>created lambda to create invalidation test</p>
+        <p></p>
         <a
           className="App-link"
           href="https://reactjs.org"
