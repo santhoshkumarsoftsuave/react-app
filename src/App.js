54835be2784735a7b4d80d89ad1cf54a883f2b2e
santhoshkumar <santhoshkumar.elumalai@softsuave.com>
2023-06-16 16:52:21 +0530

    new commit for cloudfont

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>hello world</p>
+        <p>cloudfont url</p>
         <a
           className="App-link"
           href="https://reactjs.org"
